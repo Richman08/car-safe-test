c865987a236d665e36cb988c485cb6489ef7c8c7
Richman08 <oloktionov1985@gmail.com>
2019-03-28 00:25:47 +0200

    modified menu, preload all images change arrow selector

--- js/index.js
+++ js/index.js
@@ -4,111 +4,94 @@ $(() => {
 
 	var carClassesSlide = [
 		{
-			'class': [
-							{
-								'imgUrl': './assets/image/citroen/minimum.jpg'
-							},
-							{
-								'imgUrl': './assets/image/citroen/standart.jpg'
-							},
-							{
-								'imgUrl': './assets/image/citroen/standart-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/citroen/standart-plus-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/citroen/standart-plus-plus-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/citroen/wholly.jpg'
-							},
-							{
-								'imgUrl': './assets/image/citroen/pure.jpg'
-							},
-						],
+			carClass: 'mini',
+			imgUrl: ['./assets/image/citroen/minimum.jpg', './assets/image/citroen/standart.jpg',
+					'./assets/image/citroen/standart-plus.jpg', './assets/image/citroen/standart-plus-plus.jpg',
+					'./assets/image/citroen/standart-plus-plus-plus.jpg', './assets/image/citroen/wholly.jpg',
+					'./assets/image/citroen/pure.jpg'],
+			partsImgUrl : ['./assets/image/citroen/headlights.jpg', './assets/image/citroen/threshold-inner.jpg',
+						'./assets/image/citroen/threshold-outer.jpg', './assets/image/citroen/doors.jpg',
+						'./assets/image/citroen/back-fender.jpg', './assets/image/citroen/hood.jpg',
+						'./assets/image/citroen/front-bumper.jpg', './assets/image/citroen/back-bumper.jpg']
 		},				
 		{
-			'class': [
-							{
-								'imgUrl': './assets/image/mazda/minimum.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mazda/standart.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mazda/standart-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mazda/standart-plus-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mazda/standart-plus-plus-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mazda/wholly.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mazda/pure.jpg'
-							},
-						],
+			carClass: 'midle',
+			imgUrl: ['./assets/image/mazda/minimum.jpg', './assets/image/mazda/standart.jpg',
+					'./assets/image/mazda/standart-plus.jpg', './assets/image/mazda/standart-plus-plus.jpg',
+					'./assets/image/mazda/standart-plus-plus-plus.jpg', './assets/image/mazda/wholly.jpg',
+					'./assets/image/mazda/pure.jpg'],
+			partsImgUrl : ['./assets/image/mazda/headlights.jpg', './assets/image/mazda/threshold-inner.jpg',
+						'./assets/image/mazda/threshold-outer.jpg', './assets/image/mazda/doors.jpg',
+						'./assets/image/mazda/back-fender.jpg', './assets/image/mazda/hood.jpg',
+						'./assets/image/mazda/front-bumper.jpg', './assets/image/mazda/back-bumper.jpg']
 		},
 		{
-			'class': [
-							{
-								'imgUrl': './assets/image/mercedes/minimum.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mercedes/standart.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mercedes/standart-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mercedes/standart-plus-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mercedes/standart-plus-plus-plus.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mercedes/wholly.jpg'
-							},
-							{
-								'imgUrl': './assets/image/mercedes/pure.jpg'
-							},
-						]
-		}
+			carClass: 'premium',
+			imgUrl: ['./assets/image/mercedes/minimum.jpg', './assets/image/mercedes/standart.jpg',
+					'./assets/image/mercedes/standart-plus.jpg', './assets/image/mercedes/standart-plus-plus.jpg',
+					'./assets/image/mercedes/standart-plus-plus-plus.jpg', './assets/image/mercedes/wholly.jpg',
+					'./assets/image/mercedes/pure.jpg'],
+			partsImgUrl : ['./assets/image/mercedes/headlights.jpg', './assets/image/mercedes/threshold-inner.jpg',
+						'./assets/image/mercedes/threshold-outer.jpg', './assets/image/mercedes/doors.jpg',
+						'./assets/image/mercedes/back-fender.jpg', './assets/image/mercedes/hood.jpg',
+						'./assets/image/mercedes/front-bumper.jpg', './assets/image/mercedes/back-bumper.jpg']
+		},
 	];
 
 	var indexMenu = 0;
 	var indexServicePlan = 3;
-	var arrowDown = $('.selector-arrow-down');
-	var indexCarClass = 2;
+	var indexCarClass = 0;
+	var indexPart = 0;
+	var indexPartMenu = 0;
+
+	var images = new Array;
+	function preload(url) {
+	    for (var i = 0; i < url.length; i++) {
+	        images[i] = new Image()
+	        images[i].src = url[i]
+	    }
+	};
+	preload (['./assets/image/citroen/minimum.jpg', './assets/image/citroen/standart.jpg',
+				'./assets/image/citroen/standart-plus.jpg', './assets/image/citroen/standart-plus-plus.jpg',
+				'./assets/image/citroen/standart-plus-plus-plus.jpg', './assets/image/citroen/wholly.jpg',
+				'./assets/image/citroen/pure.jpg', './assets/image/citroen/headlights.jpg', './assets/image/citroen/threshold-inner.jpg',
+				'./assets/image/citroen/threshold-outer.jpg', './assets/image/citroen/doors.jpg',
+				'./assets/image/citroen/back-fender.jpg', './assets/image/citroen/hood.jpg',
+				'./assets/image/citroen/front-bumper.jpg', './assets/image/citroen/back-bumper.jpg', './assets/image/mazda/minimum.jpg', './assets/image/mazda/standart.jpg',
+				'./assets/image/mazda/standart-plus.jpg', './assets/image/mazda/standart-plus-plus.jpg',
+				'./assets/image/mazda/standart-plus-plus-plus.jpg', './assets/image/mazda/wholly.jpg',
+				'./assets/image/mazda/pure.jpg', './assets/image/mazda/headlights.jpg', './assets/image/mazda/threshold-inner.jpg',
+				'./assets/image/mazda/threshold-outer.jpg', './assets/image/mazda/doors.jpg',
+				'./assets/image/mazda/back-fender.jpg', './assets/image/mazda/hood.jpg',
+				'./assets/image/mazda/front-bumper.jpg', './assets/image/mazda/back-bumper.jpg', './assets/image/mercedes/minimum.jpg', './assets/image/mercedes/standart.jpg',
+				'./assets/image/mercedes/standart-plus.jpg', './assets/image/mercedes/standart-plus-plus.jpg',
+				'./assets/image/mercedes/standart-plus-plus-plus.jpg', './assets/image/mercedes/wholly.jpg',
+				'./assets/image/mercedes/pure.jpg', './assets/image/mercedes/headlights.jpg', './assets/image/mercedes/threshold-inner.jpg',
+				'./assets/image/mercedes/threshold-outer.jpg', './assets/image/mercedes/doors.jpg',
+				'./assets/image/mercedes/back-fender.jpg', './assets/image/mercedes/hood.jpg',
+				'./assets/image/mercedes/front-bumper.jpg', './assets/image/mercedes/back-bumper.jpg'
+					]);
 
 	function addClassArrowDown () {
-		$(arrowDown).eq([indexCarClass]).addClass('current-selector');
+		$('.car-block').removeClass('car-block-selector')
+		$('.car-block').eq([indexCarClass]).addClass('car-block-selector')
 	};
 	addClassArrowDown ();
-
-	function selectCarClass () {
-		$('.car-block').on('click', function (event) {
-			$(arrowDown).removeClass('current-selector');
-			indexCarClass = $(this).index();
-			addClassArrowDown ();
-			defaultServicePlan();
-			createCarSlide ();
-		});
-	};
-	selectCarClass ();
 
 	function createCarSlide () {
 		$('#main-img').replaceWith(`
 				<div class="main-img" id="main-img">
-					<img src="${carClassesSlide[indexCarClass].class[indexServicePlan].imgUrl}">
+					<img src="${carClassesSlide[indexCarClass].imgUrl[indexServicePlan]}">
 				</div>	
 			`)
 	};
 	createCarSlide ();
+
+	function addClassPartBlock () {
+		$('.car-moduls-block').removeClass('cur-car-moduls-block')
+		$('.car-moduls-block').eq([indexServicePlan]).addClass('cur-car-moduls-block')
+	}
+	addClassPartBlock ();
 
 	function defaultServicePlan () {
 		$('#service-plans li').removeClass('current-plan-hover');
@@ -118,7 +101,18 @@ $(() => {
 	}
 	defaultServicePlan();
 
-	var menuItem = $('#service-plans ul').children('.menuItem')
+	function selectCarClass () {
+		$('.car-block').on('click', function (event) {
+			$('.selector-arrow-down').removeClass('current-selector');
+			indexCarClass = $(this).index();
+			addClassArrowDown ();
+			defaultServicePlan();
+			createCarSlide ();
+		});
+	};
+	selectCarClass ();
+
+	var menuItem = $('#service-plans ul').children('.menuItem');
 
 	function addClassCurrentMenuItem () {
 		$(menuItem).removeClass('current-plan-hover');
@@ -127,105 +121,49 @@ $(() => {
 		$('#service-plans span').eq([indexMenu]).addClass('current-item-menu');
 	};
 
-	$(menuItem).on('mouseover', function(event) {
-		indexMenu = $(this).index();
-		addClassCurrentMenuItem ();
-		indexServicePlan = $(this).index();
-		createCarSlide ();
-	});
-
-	function activeInPartsMenu () {
-		$('#in-part-item span, i').toggleClass('in-part-active');
-		$('#in-parts-dropdown-hover').toggleClass('in-parts-dropdown-hover');
-		$('#fa-angle-down').hasClass('in-part-active') 
-				? $('#fa-angle-down').addClass('fa-angle-down-rotate')
-				: $('#fa-angle-down').removeClass('fa-angle-down-rotate');
-	};
-
 	$('#in-part-item').on('click', function(event) {
 		event.preventDefault();
-		activeInPartsMenu ();
+		$('#fa-angle-down').toggleClass('fa-angle-down-rotate')
 		$('.list-car-parts').slideToggle();
 	});
 
-	var carParts = [
-		{
-			'id': 'headlight',
-			'imgPartsUrl' : './assets/image/car-moduls/headlight.png',
-			'title': 'Фары'
-
-		},
-		{
-			'id': 'threshold',
-			'imgPartsUrl' : './assets/image/car-moduls/threshold.png',
-			'title': 'Пороги'
-		},
-		{
-			'id': 'mirror',
-			'imgPartsUrl' : './assets/image/car-moduls/mirror.png',
-			'title': 'Зеркала'
-		},
-		{
-			'id': 'doors',
-			'imgPartsUrl' : './assets/image/car-moduls/door.png',
-			'title': 'Двери'
-		},
-		{
-			'id': 'fender',
-			'imgPartsUrl' : './assets/image/car-moduls/fender.png',
-			'title': 'Крылья'
-		},
-		{
-			'id': 'hood',
-			'imgPartsUrl' : './assets/image/car-moduls/hood.png',
-			'title': 'Капот'
-		},
-		{
-			'id': 'front-bumper',
-			'imgPartsUrl' : './assets/image/car-moduls/front-bumper.png',
-			'title': 'Передний бампер'
-		},
-		{
-			'id': 'back-bumper',
-			'imgPartsUrl' : './assets/image/car-moduls/back-bumper.png',
-			'title': 'Задний бампер'
-		},
-	];
-
-	function addCarPart (index) {
-		for (var item of carParts) {
-			if (index === item.id) { 	 	
-				$('#car-moduls-wraper').append(`
-				<div  class="${item.id}">
-					<div class="moduls-img">
-						<img src="${item.imgPartsUrl}">
-					</div>
-					<div class="moduls-title">
-						<p>${item.title}</p>
-					</div>
+	function createPartSlide () {
+		$('#main-img').replaceWith(`
+				<div class="main-img" id="main-img">
+					<img src="${carClassesSlide[indexCarClass].partsImgUrl[indexPart]}">
 				</div>	
-				`)
-			}
-		}
+			`)
 	};
 
-	$('#list-car-parts li').on('click', function(event) {
-		event.preventDefault();
-		console.log($('#car-moduls-wraper').children().attr('class') == event.currentTarget.id)
-			if ($('#car-moduls-wraper').children().attr('class') != event.currentTarget.id) {
-				addCarPart (event.currentTarget.id)
-			} else {
-				
-				$(`div.${event.currentTarget.id}`).remove()
-			}
+	var menuParts = $('#list-car-parts').children('.parts');
+
+	function addClassCurrentPart () {
+		$(menuParts).removeClass('list-car-part');
+		$('#list-car-parts a').removeClass('current-item-menu');
+		$(menuParts).eq([indexPartMenu]).addClass('list-car-part');
+		$('#list-car-parts a').eq([indexPartMenu]).addClass('current-item-menu');	
+	};
+	
+	$('#service-plans ul li span').on('mouseover', function(event) {	
+		if ($(this).parent().hasClass('menuItem')) {
+			$(menuParts).removeClass('list-car-part');
+			$('#list-car-parts a').removeClass('current-item-menu');
+			indexMenu = $(this).parent().index();
+			addClassCurrentMenuItem ();
+			indexServicePlan = $(this).parent().index();
+			createCarSlide ();
+			addClassPartBlock ();
+		}
 	});
 
-
-
-
-	
-
-
+	$('#list-car-parts li a').on('mouseover', function(event) {	
+		$(menuItem).removeClass('current-plan-hover');
+		$('#service-plans span').removeClass('current-item-menu');
+		indexPartMenu = $(this).parent().index();
+		addClassCurrentPart ();
+		indexPart = $(this).parent().index();
+		createPartSlide ();
+	});
 
 
 });
